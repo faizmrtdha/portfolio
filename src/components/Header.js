0aecddfe4713c99627e314e31714cbcaca29f6a4
Faiz Murtadha Nur Wahhab <faizmrtdha99@gmail.com>
2021-12-09 18:15:39 +0700

    remove typed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,10 @@
 import React from "react";
-import Typed from "react-typed";
 
 const Header = () => {
   return (
     <div className="header-wrapper">
       <div className="main-info">
         <h1>Web Development</h1>
-        <Typed
-          className="typed-text"
-          strings={["Web Design", "Web Development", "Facebook Ads"]}
-          typeSpeed={40}
-          backSpeed={60}
-          loop
-        />
         <a href="javascript:void(0)" className="btn-main-offer">
           Contact Me
         </a>
